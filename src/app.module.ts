c116ea4bbd7a3faf54a8e2fcb2f489b5894f7814
muje hoxe <mujehoxe@gmail.com>
2022-11-08 23:06:47 +0100

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoursesModule } from './courses/courses.module';
+import { TopicsModule } from './topics/topics.module';
 
 
 @Module({
-  imports: [],
+  imports: [CoursesModule, TopicsModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'secret',
+    database: 'edu',
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
