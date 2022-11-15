68af5bc01c0278ccac5037e398fac0ddd8fa031e
muje hoxe <mujehoxe@gmail.com>
2022-11-15 14:40:49 +0100

    Init frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesModule } from './courses/courses.module';
 import { TopicsModule } from './topics/topics.module';
 import { LocalFilesModule } from './local-files/local-files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'secret',
-      database: 'edu',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    CoursesModule,
-    TopicsModule,
-    LocalFilesModule,
-  ],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 5432,
+			username: 'postgres',
+			password: 'secret',
+			database: 'edu',
+			autoLoadEntities: true,
+			synchronize: true,
+		}),
+		ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '../..', 'public/dist'),
+		}),
+		CoursesModule,
+		TopicsModule,
+		LocalFilesModule,
+	],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	app.enableCors({ origin: '*' });
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
+
+	await app.listen(3000);
 }
 bootstrap();
