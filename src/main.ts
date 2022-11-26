f75a86ff46e2d8cde70948517f22853514ec6b5a
muje hoxe <mujehoxe@gmail.com>
2022-11-26 21:57:16 +0100

    exclude uploads

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,7 +6,11 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.enableCors({ origin: '*' });
-	app.setGlobalPrefix('/v1');
+
+	app.setGlobalPrefix('v1', {
+		exclude: [{ path: 'uploads', method: RequestMethod.GET }],
+	});
+
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true,
