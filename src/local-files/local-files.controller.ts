a7a74020e393996d5ec3c61a5e4e92031032cf6a
muje hoxe <mujehoxe@gmail.com>
2023-03-11 14:25:18 +0100

    Serve video in chunks

--- src/local-files/local-files.controller.ts
+++ src/local-files/local-files.controller.ts
@@ -5,36 +5,58 @@ import {
 	Res,
 	Param,
 	VERSION_NEUTRAL,
+	Headers,
+	HttpStatus,
 } from '@nestjs/common';
 
 import { Magic, MAGIC_MIME_TYPE } from 'mmmagic';
 
-import { createReadStream } from 'fs';
+import { createReadStream, stat } from 'fs';
 import { join } from 'path';
 import type { Response } from 'express';
+import { promisify } from 'util';
+import { ReadStream } from 'typeorm/platform/PlatformTools';
+
+const fileInfo = promisify(stat);
 
 @Controller({ path: 'uploads', version: VERSION_NEUTRAL })
 export class LocalFilesController {
 	@Get(':filetype/:filename')
 	async getFile(
+		@Headers() headers,
 		@Param('filetype') type: string,
 		@Param('filename') filename: string,
 		@Res({ passthrough: true }) res: Response,
 	): Promise<StreamableFile> {
 		const path = './uploads/' + type + '/' + filename;
 
-		const file = createReadStream(join(process.cwd(), path));
-
 		try {
 			const mimeType = await detectFilePromise(path);
 
 			res.set({
 				'Content-Type': mimeType || 'application/octet-stream',
-				// 'Content-Disposition': 'attachment; filename="package.json"',
 			});
 		} catch (err) {
 			console.log(err);
 		}
+
+		const { size } = await fileInfo(path);
+		const range = headers.range;
+
+		let file: ReadStream;
+
+		if (range) {
+			const parts = range.replace(/bytes=/, '').split('-');
+			const start = parseInt(parts[0], 10);
+			const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
+			const chunksize = end - start + 1;
+
+			file = createReadStream(path, { start, end, highWaterMark: 60 });
+			res.writeHead(HttpStatus.PARTIAL_CONTENT, {
+				'Content-Range': `bytes ${start}-${end}/${size}`,
+				'Content-Length': chunksize,
+			});
+		} else file = createReadStream(join(process.cwd(), path));
 
 		return new StreamableFile(file);
 	}
